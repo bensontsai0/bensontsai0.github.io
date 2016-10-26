8fb0e830acd214ffdc476b24702874885a6c7a08
BensonTsai0 <bensontsai0968@gmail.com>
2016-10-26 17:35:31 +0800

    V5.01

--- chance/js/global.js
+++ chance/js/global.js
@@ -1,4 +1,14 @@
+var pathname = window.location.pathname; // Returns path only
+var url      = window.location.href;     // Returns full URL
+
 $(function(){
   	$("#header").load("pageHeader.html");
-  	$("#footer").load("pageFooter.html"); 
+  	$("#footer").load("pageFooter.html");
 });
+
+function clickMenu(obj){
+	var url2 = $(obj).attr('href');
+	console.log(pathname);
+	console.log(url);
+	console.log(url2);
+}
