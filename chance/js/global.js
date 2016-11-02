ad7d45b971de61d79924910235c83e55fa42cd1f
bensontsai0 <bensontsai0968@gmail.com>
2016-11-03 02:00:51 +0800

    V2.11 scroll

--- chance/js/global.js
+++ chance/js/global.js
@@ -1,5 +1,6 @@
 var pathname = window.location.pathname; // Returns path only
 var url      = window.location.href;     // Returns full URL
+var count = 0;
 var $body = (window.opera) ? (document.compatMode == "CSS1Compat" ? $('html') : $('body')) : $('html,body');
 
 $(function(){
@@ -12,15 +13,18 @@ function clickMenu(obj){
 	if(pathname.indexOf('index.html') == -1){
 		window.location = "index.html";
 	} else {
-		//控制nav點擊縮放
-		$("ul.nav.navbar-nav li.dropdown ul.dropdown-menu li a").click(function(){
-            var id = $(this).attr('id');
-            $(".navbar-collapse.collapse").removeClass("in");
-            //scroll
-            $body.animate({
-              scrollTop: $(id).offset().top
-            }, 1500);
-        });
+		if(count == 0){
+			//控制nav點擊縮放
+			$("ul.nav.navbar-nav li.dropdown ul.dropdown-menu li a").click(function(){
+	            var id = $(this).attr('id');
+	            $(".navbar-collapse.collapse").removeClass("in");
+	            //scroll
+	            $body.animate({
+	              scrollTop: $(id).offset().top
+	            }, 1500);
+	        });
+	        count++;
+		}
 	}
 }
 
